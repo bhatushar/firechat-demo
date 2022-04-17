b8c5014f0d1ca707ad0862befeb5343b88958ebe
Tushar Bhatt <tbhatt321@gmail.com>
2022-04-17 19:53:21 +0530

    Added support for default profile photo

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,3 +1,4 @@
+import defaultProfilePhoto from "../assets/default-profile-picture.jpg";
 interface ChatProps {
   type: "sent" | "received";
   photoUrl?: string;
@@ -12,7 +13,11 @@ function Chat({ type, photoUrl, message }: ChatProps) {
       }`}
     >
       {type === "received" && (
-        <img src={photoUrl} alt="Photo" className="rounded-full w-9 h-9 mt-2" />
+        <img
+          src={photoUrl ?? defaultProfilePhoto}
+          alt="Photo"
+          className="rounded-full w-9 h-9 mt-2"
+        />
       )}
       <div
         className={`px-2 py-1 rounded-2xl mt-2 ${
